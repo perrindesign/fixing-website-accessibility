a9b31c681c6e4f0dbd181a98eb0d3bd67c7e25c9
Perrin Anto <paj2117@columbia.edu>
2020-02-23 19:11:54 -0500

    Fixing links, forms, and focus

--- project2.js
+++ project2.js
@@ -1,48 +1,22 @@
 $(document).ready(function() {
-  // Toggle the receive spam checkbox
-  $("#receivespambutton").click(function() {
-
-    if($($("#receivespambutton").children()[0]).attr("src")=="pics/unchecked.png") {
-      $($("#receivespambutton").children()[0]).attr("src","pics/checked.png");
-      $("#spamyn").val("y");
-    } else {
-      $($("#receivespambutton").children()[0]).attr("src","pics/unchecked.png");
-      $("#spamyn").val("n");
-    }
-  });
-
-
-  // Play video
-  $("#videoplayer").mousedown(function() {
-
-    if($($("#videoplayer").children()[0]).attr("src")=="pics/play.png") {
-      $($("#videoplayer").children()[0]).attr("src","pics/pause.jpg");
-      $("#thevideo")[0].play();
-    } else {
-      $($("#videoplayer").children()[0]).attr("src","pics/play.png");
-      $("#thevideo")[0].pause();
-    }
-  });
-
   // Form validation
   $("#signupbutton").click(function() {
-    if($("#fn").val()=="" || $("#mi").val()=="" || $("#ln").val()=="") {
-      $("#name").addClass("error");
-
-      return;
-    } else {
-      $("#name").removeClass("error");
+    $("input").removeClass("error"); //remove all existing error classes and recheck form fields
+    if($("#first-name").val()=="") { //mark fields that have not been filled out
+      $("#first-name").addClass("error");
+    } 
+    if($("#last-name").val()=="") {
+      $("#last-name").addClass("error");
     }
-
-    if($("#em").val()=="") {
+    if($("#email").val()=="") {
       $("#email").addClass("error");
-
-      return;
-    } else {
-      $("#name").removeClass("error");
     }
-
-    alert("Thank you!  Please watch your email for our exciting newsletter and offers!");
+    if($("#first-name").val()=="" || $("#last-name").val()=="" || $("#email").val()=="") { //if a required field is missing, alert user
+      alert("Please go back and fill out first name, last name, and email before signing up.");
+    } else {
+      $("input").removeClass("error");
+      alert("Thank you! Please watch your email for our exciting newsletter and offers!"); //else, confirm form has been recieved 
+    }
   });
 });
 
